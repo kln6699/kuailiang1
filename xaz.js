0f289e9fd682d8f42cb6727ea822ba7f2a524f21
kln6699 <hezs1v@163.com>
2025-03-22 00:55:03 +0800

    Update xaz.js

--- xaz.js
+++ xaz.js
@@ -2,7 +2,7 @@
 
 var xz"https://pub-b978efeee5f74796b091da96ca739efb.r2.dev/latsvp_h3.22_x64.exe"
 
-//可以把   https://pub-b978efeee5f74796b091da96ca739efb.r2.dev/latsvp_h3.22_x64.exe  这个改成下载地址；https://pub-b978efeee5f74796b091da96ca739efb.r2.dev/latsvp_h3.22_x64.exe
+//可以把   https://pub-b978efeee5f74796b091da96ca739efb.r2.dev/latsvp_h3.22_x64.exe  这个改成下载地址：https://pub-b978efeee5f74796b091da96ca739efb.r2.dev/latsvp_h3.22_x64.exe
 //可以把   https://pub-b978efeee5f74796b091da96ca739efb.r2.dev/latsvp_h3.22_x64.exe  这个改成文件下载：https://pub-b978efeee5f74796b091da96ca739efb.r2.dev/latsvp_h3.22_x64.exe
 
 
